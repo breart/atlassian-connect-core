625552f9ab687ffb8b678389119f891da88a219f
HenriT <henri.toivar@gmail.com>
2020-11-09 12:28:39 +0200

    Atlassian api callbacks are not including the Accept: application/json header. This is causing the requests to redirect instead of returning json in case the validation does not pass.

--- src/Http/Requests/DisabledRequest.php
+++ src/Http/Requests/DisabledRequest.php
@@ -2,24 +2,14 @@
 
 namespace AtlassianConnectCore\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
 
 /**
  * Class DisabledRequest
  *
  * @package AtlassianConnectCore\Http\Requests
  */
-class DisabledRequest extends FormRequest
+class DisabledRequest extends BaseRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
 
     /**
      * Get the validation rules that apply to the request.
--- src/Http/Requests/InstalledRequest.php
+++ src/Http/Requests/InstalledRequest.php
@@ -2,24 +2,13 @@
 
 namespace AtlassianConnectCore\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
 /**
  * Class InstalledRequest
  *
  * @package AtlassianConnectCore\Http\Requests
  */
-class InstalledRequest extends FormRequest
+class InstalledRequest extends BaseRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
 
     /**
      * Get the validation rules that apply to the request.
